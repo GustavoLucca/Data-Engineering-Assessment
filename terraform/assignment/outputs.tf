6944073249ce62287b371bc994aa64e80b108e66
Gustavo Lucca <95438755+GustavoLucca@users.noreply.github.com>
2026-03-16 12:54:32 -0500

    Added input and output bucket names as outputs

--- terraform/assignment/outputs.tf
+++ terraform/assignment/outputs.tf
@@ -5,3 +5,11 @@ output "ecr_repository_url" {
 output "lambda_function_name" {
   value = "${local.app_name}-file-processor"
 }
+
+output "input_bucket_name" {
+  value = aws_s3_bucket.input_s3.bucket
+}
+
+output "output_bucket_name" {
+  value = aws_s3_bucket.output_s3.bucket
+}
